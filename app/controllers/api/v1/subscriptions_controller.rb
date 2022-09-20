6ffe2d288e4c4422418a4d6b1f57f1e6b8a70ceb
Colin Reinhart <colinbreinhart@gmail.com>
2022-09-20 11:43:59 -0600

    Subscription Controller Create

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def create
-    require "pry"; binding.pry
+    sub_create = Subscription.new(sub_params)
+    if sub_create.save
+      render json: SubscriptionSerializer.new(sub_create), status: 201
+    else
+      render json: { error: "Error" }, status: 401
+    end
   end
+
+  private
+    def sub_params
+      params.permit(:title, :price, :status, :customer_id, :tea_id)
+    end
 
 end
