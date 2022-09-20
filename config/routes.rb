d2776740a7c64720bf556650c346f9be75d2b459
Colin Reinhart <colinbreinhart@gmail.com>
2022-09-20 16:52:13 -0600

    Index Customer Subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: %i[create destroy]
+      resources :customers, only: %i[]do
+        resources :subscriptions, only: %i[index]
+      end
     end
   end
 end
--- spec/requests/api/v1/subscription_spec.rb
+++ spec/requests/api/v1/subscription_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe 'Subscription request' do
       @customer_3 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, address: Faker::Address.full_address)
 
       @tea_1 = Tea.create!(title: Faker::Tea.variety, description: Faker::ChuckNorris.fact, temperature: Faker::Number.between(from: 190, to: 220), brew_time: Faker::Number.between(from: 8, to: 16))
-      @tea_3 = Tea.create!(title: Faker::Tea.variety, description: Faker::ChuckNorris.fact, temperature: Faker::Number.between(from: 190, to: 220), brew_time: Faker::Number.between(from: 8, to: 16))
+      @tea_2 = Tea.create!(title: Faker::Tea.variety, description: Faker::ChuckNorris.fact, temperature: Faker::Number.between(from: 190, to: 220), brew_time: Faker::Number.between(from: 8, to: 16))
       @tea_3 = Tea.create!(title: Faker::Tea.variety, description: Faker::ChuckNorris.fact, temperature: Faker::Number.between(from: 190, to: 220), brew_time: Faker::Number.between(from: 8, to: 16))
 
-      @sub_1 = Subscription.create!(title: "#{@customer_1.first_name}'s Sub", price: Faker::Number.between(from: 20, to: 50), status: "active", frequency: "weekly", customer: @customer_1, tea: @tea_1)
+      @sub_1 = Subscription.create!(title: "#{@customer_1.first_name}'s First Sub", price: Faker::Number.between(from: 20, to: 50), status: "active", frequency: "weekly", customer: @customer_1, tea: @tea_1)
+      @sub_2 = Subscription.create!(title: "#{@customer_1.first_name}'s Second Sub", price: Faker::Number.between(from: 20, to: 50), status: "canceled", frequency: "weekly", customer: @customer_1, tea: @tea_2)
+      @sub_3 = Subscription.create!(title: "#{@customer_2.first_name}'s First Sub", price: Faker::Number.between(from: 20, to: 50), status: "active", frequency: "weekly", customer: @customer_2, tea: @tea_3)
     end
 
     it "creates a new subscription for a customer" do
@@ -46,6 +48,18 @@ RSpec.describe 'Subscription request' do
 
       body = JSON.parse(response.body, symbolize_names: true)
       expect(body[:data][:attributes][:status]).to eq("canceled")
+    end
+
+    it "can return all of one customers subscriptions regardless of status" do
+
+      get "/api/v1/customers/#{@customer_1.id}/subscriptions"
+      body = JSON.parse(response.body, symbolize_names: true)
+      data = body[:data]
+      expect(data.count).to eq(2)
+      expect(data.first[:attributes]).to have_key(:title)
+      expect(data.first[:attributes]).to have_key(:price)
+      expect(data.first[:attributes]).to have_key(:status)
+      expect(data.first[:attributes]).to have_key(:frequency)
     end
   end
 end
